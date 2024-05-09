b3ef27ccac4de189511a1702a4668f1cda7e1438
MikeKovalevskiy <mishanjakovalevskiy@gmail.com>
2024-05-09 12:16:50 +0300

    adding password generation

--- Password_generator/main.cpp
+++ Password_generator/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    srand(time(0));
     MainWindow w;
     w.show();
 
--- Password_generator/mainwindow.cpp
+++ Password_generator/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+//#include <QRegExp>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,39 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    int passwordLength = ui->spinBox->value();
+    bool isLower = ui->checkBox->isChecked();
+    bool isUpper = ui->checkBox_upper->isChecked();
+    bool isNumbers = ui->checkBox_numbers->isChecked();
+    bool isSymbols = ui->checkBox_symbols->isChecked();
+
+    QString letters = "abcdefghijklmnopqrstuvwxyz";
+    QString upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    QString numbers = "0123456789";
+    QString symbols = "!@#$%^&*()~,./;:?[]{}-_+=|<>";
+    QString password;
+
+    for(int i = 0; i < passwordLength; i++)
+    {
+        if (password.length() < passwordLength)
+        {
+            if(isLower)
+                password.push_back(letters[rand() % 26]);
+
+            if(isUpper)
+                password.push_back(upperLetters[rand() % 26]);
+
+            if(isNumbers)
+                password.push_back(numbers[rand() % 10]);
+
+            if(isSymbols)
+                password.push_back(symbols[rand() % 28]);
+        }
+    }
+
+    ui->lineEdit->setText(password);
 }
