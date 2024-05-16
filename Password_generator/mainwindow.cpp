ecb4904b9d06401ef7523466de34c5465a10dece
MikeKovalevskiy <mishanjakovalevskiy@gmail.com>
2024-05-16 11:20:59 +0300

    adding password shuffle and random filler

--- Password_generator/mainwindow.cpp
+++ Password_generator/mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 //#include <QRegExp>
+#include <QMessageBox>
+#include <algorithm>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -22,28 +24,84 @@ void MainWindow::on_pushButton_clicked()
     bool isNumbers = ui->checkBox_numbers->isChecked();
     bool isSymbols = ui->checkBox_symbols->isChecked();
 
+    int amountOfLowerLetters = ui->spinBox_lower->value();
+    int amountOfUpperLetters = ui->spinBox_upper->value();
+    int amountOfNumberLetters = ui->spinBox_numbers->value();
+    int amountOfSymbolLetters = ui->spinBox_symbols->value();
+
     QString letters = "abcdefghijklmnopqrstuvwxyz";
     QString upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     QString numbers = "0123456789";
     QString symbols = "!@#$%^&*()~,./;:?[]{}-_+=|<>";
     QString password;
 
-    for(int i = 0; i < passwordLength; i++)
+    if(!(ui->spinBox->value() >= amountOfLowerLetters + amountOfUpperLetters +
+            amountOfNumberLetters + amountOfSymbolLetters))
     {
+        QMessageBox msgBox;
+        msgBox.setText("Число конкретних символів не може перевищувати загальну довжину паролю!");
+        msgBox.setStandardButtons(QMessageBox::Ok | QMessageBox::Cancel);
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        int ret = msgBox.exec();
+    }
+    else {
+        for(int i = 0; i < passwordLength; i++)
+        {
+            if (password.length() < passwordLength)
+            {
+                if(isLower && amountOfLowerLetters > 0)
+                {
+                    password.push_back(letters[rand() % 26]);
+                    amountOfLowerLetters--;
+                }
+
+                if(isUpper && amountOfUpperLetters > 0)
+                {
+                    password.push_back(upperLetters[rand() % 26]);
+                    amountOfUpperLetters--;
+                }
+
+                if(isNumbers && amountOfNumberLetters > 0)
+                {
+                    password.push_back(numbers[rand() % 10]);
+                    amountOfNumberLetters--;
+                }
+
+                if(isSymbols && amountOfSymbolLetters > 0)
+                {
+                    password.push_back(symbols[rand() % 28]);
+                    amountOfSymbolLetters--;
+                }
+            }
+        }
+
         if (password.length() < passwordLength)
         {
-            if(isLower)
-                password.push_back(letters[rand() % 26]);
+            for(int i = 0; i <= passwordLength - password.length(); i++)
+            {
+                if(isLower && password.length() < passwordLength)
+                {
+                    password.push_back(letters[rand() % 26]);
+                }
 
-            if(isUpper)
-                password.push_back(upperLetters[rand() % 26]);
+                if(isUpper && password.length() < passwordLength)
+                {
+                    password.push_back(upperLetters[rand() % 26]);
+                }
 
-            if(isNumbers)
-                password.push_back(numbers[rand() % 10]);
+                if(isNumbers && password.length() < passwordLength)
+                {
+                    password.push_back(numbers[rand() % 10]);
+                }
 
-            if(isSymbols)
-                password.push_back(symbols[rand() % 28]);
+                if(isSymbols && password.length() < passwordLength)
+                {
+                    password.push_back(symbols[rand() % 28]);
+                }
+            }
         }
+
+        std::random_shuffle(std::begin(password), std::end(password));
     }
 
     ui->lineEdit->setText(password);
